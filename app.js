c7b64c2cd6866c2626c6d6f51a95ed97946448ac
EC2 Default User <ec2-user@ip-172-31-37-190.us-east-2.compute.internal>
2020-03-23 05:08:02 +0000

    done

--- app.js
+++ app.js
@@ -3,14 +3,23 @@ const app = express();
 app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 //routes
-app.get("/", function(req, res){
+app.get("/index", function(req, res){
     res.render("index.html");
 });
 app.get("/mercury", function(req, res){
-   res.send("This will be Mercury web page!");
+   res.render("mercury.html");
 });
 app.get("/venus", function(req, res){
-   res.send("This will be Venus web page!");
+   res.render("venus.html");
+});
+app.get("/earth", function(req, res){
+   res.render("earth.html");
+});
+app.get("/mars", function(req, res){
+   res.render("mars.html");
+});
+app.get("/jupiter", function(req, res){
+   res.render("jupiter.html");
 });
 //server listener
 app.listen(process.env.PORT, process.env.IP, function(){
